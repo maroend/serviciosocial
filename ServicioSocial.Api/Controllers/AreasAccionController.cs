ab57c603e84fb36e8e738f98fad489d4fd27ab3b
secahu <47434715+secahu@users.noreply.github.com>
2020-06-23 18:41:34 -0500

    organizaciones api

---
+++ ServicioSocial.Api/Controllers/AreasAccionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServicioSocial.Business.Models;
+using ServicioSocial.Business.Repositories.Interfaces;
+
+namespace ServicioSocial.Api.Controllers
+{
+    public class AreasAccionController : GenericController<AreasAccion>
+    {
+        private readonly IAreasAccionRepository _repository;
+
+        public AreasAccionController(IAreasAccionRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+
+    }
+}
