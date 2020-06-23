7106d3745d5252c3ab2129efab100d49c4101420
secahu <47434715+secahu@users.noreply.github.com>
2020-06-23 18:38:37 -0500

    organizaciones interfaces

---
+++ ServicioSocial.Business/Repositories/Interfaces/IContactosRepository.cs
@@ -0,0 +1,14 @@
+﻿using ServicioSocial.Business.Generic;
+using ServicioSocial.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioSocial.Business.Repositories.Interfaces
+{
+    public interface IContactosRepository : IGenericRepository<Contactos>
+    {
+        Task<IEnumerable<Contactos>> GetListByIdOrganizacion(long idOrganizacion);
+    }
+}
---
+++ ServicioSocial.Business/Repositories/Interfaces/IIntegrantesPatronatoRepository.cs
@@ -0,0 +1,14 @@
+﻿using ServicioSocial.Business.Generic;
+using ServicioSocial.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicioSocial.Business.Repositories.Interfaces
+{
+    public interface IIntegrantesPatronatoRepository : IGenericRepository<IntegrantesPatronato>
+    {
+        Task<IEnumerable<IntegrantesPatronato>> GetLisByIdOrganizacion(long idOrganizacion);
+    }
+}
